ad305b20ab40b7ec6e58fbdf81bb89ef91ebb054
lepfeiffenberger <90902624+lepfeiffenberger@users.noreply.github.com>
2024-11-15 11:08:28 +0100

    added DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button class="btn">DaisyUI</button>
